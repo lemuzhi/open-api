c49bdfa30c1119e180b4f2a62ed3a7ab4213f34b
lemuzhi <1163648924@qq.com>
2023-05-08 21:23:42 +0800

    完成短视频挂载接口

--- test/run_test.go
+++ test/run_test.go
@@ -15,7 +15,8 @@ const (
 
 func TestRun(t *testing.T) {
 	tk := tiktok_open_api.NewTiktokOpenApi(appid, secret, salt)
-	res, err := tk.Jscode2session(code)
+	//res, err := tk.Jscode2session(code)
+	res, err := tk.ShortVideoMount("0801121846313073694773626f3359725a507377724c542f34413d3d")
 	if err != nil {
 		fmt.Println("错误", err)
 	}
--- tiktok_open_api/14.mount.go
+++ tiktok_open_api/14.mount.go
@@ -1,3 +1,35 @@
 package tiktok_open_api
 
+import (
+	"encoding/json"
+	"github.com/lemuzhi/open-api/utils"
+	"net/http"
+)
+
 // 挂载相关接口
+
+type ShortVideoMount struct {
+	ErrNo  int    `json:"err_no"`
+	ErrMsg string `json:"err_msg"`
+	Data   struct {
+		QrcodeURL          string `json:"qrcode_url"`
+		QrcodeParseContent string `json:"qrcode_parse_content"`
+	} `json:"data"`
+}
+
+func (t *TiktokOpenApi) ShortVideoMount(accessToken string) (*ShortVideoMount, error) {
+	u := "https://developer.toutiao.com/api/apps/v1/capacity/get_self_mount_bind_qrcode"
+
+	header := map[string]string{
+		"access-token": accessToken,
+	}
+
+	body, err := utils.Request(http.MethodGet, u+"?capacity_key=video_self_mount", header, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var respData ShortVideoMount
+	err = json.Unmarshal(*body, &respData)
+	return &respData, err
+}

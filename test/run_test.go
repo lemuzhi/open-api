56691954b3bf863ef054adbd9694cd1ce4271bd6
lemuzhi <1163648924@qq.com>
2023-05-11 17:39:58 +0800

    完成预下单接口

--- test/run_test.go
+++ test/run_test.go
@@ -17,7 +17,8 @@ func TestRun(t *testing.T) {
 	tk := open_api.NewTiktokOpenApi(appid, secret, salt)
 	//res, err := tk.Jscode2session(code)
 	//res, err := tk.ShortVideoMount("0801121846313073694773626f3359725a507377724c542f34413d3d")
-	res, err := tk.OpenApi.EscrowPay.Pay.QueryOrder("7018002683625031257")
+	//res, err := tk.EscrowPay.Pay.QueryOrder("7013158683625009646")
+	res, err := tk.EscrowPay.Pay.CreateOrder("7013158683611111111", "测试", "测试1", 0.01, 300)
 	if err != nil {
 		fmt.Println("错误", err)
 	}
--- tiktok_open_api/escrow_pay/pay.go
+++ tiktok_open_api/escrow_pay/pay.go
@@ -2,7 +2,6 @@ package escrow_pay
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/lemuzhi/open-api/global"
 	"github.com/lemuzhi/open-api/utils"
 )
@@ -27,6 +26,55 @@ type QueryOrderResp struct {
 	} `json:"payment_info"`
 }
 
+type CreateOrderResp struct {
+	ErrNo   int    `json:"err_no"`
+	ErrTips string `json:"err_tips"`
+	Data    struct {
+		OrderID    string `json:"order_id"`
+		OrderToken string `json:"order_token"`
+	} `json:"data"`
+}
+
+// CreateOrder 预下单接口
+// orderNo 开发者侧的订单号
+//
+// subject 商品描述
+// body 商品详情
+// totalAmount 支付价格
+// expiredAt 订单过期时间
+
+func (p *Pay) CreateOrder(orderNo, subject, body string, totalAmount float64, expiredAt uint32) (*CreateOrderResp, error) {
+	paramsMap := map[string]interface{}{
+		"app_id":        global.AppID,      //小程序APPID
+		"thirdparty_id": "",                //第三方平台服务商 id，非服务商模式留空
+		"out_order_no":  orderNo,           //开发者侧的订单号。 只能是数字、大小写字母_-*且在同一个商户号下唯一
+		"total_amount":  totalAmount * 100, //支付价格。 单位为[分]， 取值范围： [1,10000000000]
+		"subject":       subject,           //商品描述。 长度限制不超过 128 字节且不超过 42 字符
+		"body":          body,              //商品详情 长度限制不超过 128 字节且不超过 42 字符
+		"valid_time":    expiredAt,         //订单过期时间(秒)。最小5分钟，最大2天，小于5分钟会被置为5分钟，大于2天会被置为2天，取值范围： [300,172800]，当前表示30分钟过期
+		//"notify_url":    "https://www.xxx.com", //商户自定义回调地址，必须以 https 开头，支持 443 端口。 指定时，支付成功后抖音会请求该地址通知开发者
+		//"disable_msg":   0, //是否屏蔽支付完成后推送用户抖音消息，1-屏蔽 0-非屏蔽，默认为0。 特别注意： 若接入POI, 请传1。因为POI订单体系会发消息，所以不用再接收一次担保支付推送消息，
+		//"msg_page":      "pages/user/orderDetail/orderDetail?id=997979879879879879", //支付完成后推送给用户的抖音消息跳转页面
+		"sign": "", //签名，详见签名DEMO:https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/open-capacity/guaranteed-payment/TE/#_%E4%B8%89%E7%AD%BE%E5%90%8D-demo
+	}
+	sign := utils.RequestSign(paramsMap, global.Salt)
+	paramsMap["sign"] = sign
+
+	u := "https://developer.toutiao.com/api/apps/ecpay/v1/create_order"
+
+	header := map[string]string{
+		"Content-Type": "application/json",
+	}
+
+	res, err := utils.Request("POST", u, header, paramsMap)
+	if err != nil {
+		return nil, err
+	}
+
+	var respData CreateOrderResp
+	err = json.Unmarshal(*res, &respData)
+	return &respData, err
+}
 func (p *Pay) QueryOrder(orderNo string) (*QueryOrderResp, error) {
 	//查询订单结果只有out_order_no加签
 	paramsMap := map[string]interface{}{
@@ -35,9 +83,7 @@ func (p *Pay) QueryOrder(orderNo string) (*QueryOrderResp, error) {
 		"out_order_no":  orderNo,      //开发者侧的订单号。 只能是数字、大小写字母_-*且在同一个商户号下唯一
 		"sign":          "",           //签名，详见签名DEMO:https://developer.open-douyin.com/docs/resource/zh-CN/mini-app/open-capacity/guaranteed-payment/TE/#_%E4%B8%89%E7%AD%BE%E5%90%8D-demo
 	}
-	fmt.Println("salt=", global.AppID)
 	sign := utils.RequestSign(paramsMap, global.Salt)
-	fmt.Println("sign=", sign)
 	body := map[string]interface{}{
 		"app_id":       global.AppID,
 		"out_order_no": orderNo, //开发者侧的订单号, 同一小程序下不可重复
